4548a7d997d8fc4c9edad7b771a4ed9a0da6fa91
RazikaBengana <razikabengana@gmail.com>
2024-09-30 05:39:22 +0200

    Add controllers for `home` and `jokes` management

---
+++ app/controllers/jokes_controller.rb
@@ -0,0 +1,53 @@
+class JokesController < ApplicationController
+
+  def index
+    @jokes = Joke.all
+  end
+
+  def show
+    @joke = Joke.find(params[:id])
+  end
+
+  def new
+    @joke = Joke.new
+  end
+
+  def edit
+    @joke = Joke.find(params[:id])
+  end
+
+  def create
+    @joke = Joke.new(jokes_params)
+
+    if @joke.save
+      redirect_to jokes_path
+    else
+      render :new
+    end
+
+  end
+
+  def update
+    @joke = Joke.find(params[:id])
+
+    if @joke.update(jokes_params)
+      redirect_to jokes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @joke = Joke.find(params[:id])
+    @joke.destroy
+    redirect_to jokes_path
+  end
+
+  private
+
+  def jokes_params
+    params.require(:joke).permit(:name, :content, :author)
+  end
+
+end
+
